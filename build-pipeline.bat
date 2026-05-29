7c1538cf899c6b9cf9b1ebebd7aad9aa210cf5b5
Jonathan <jonathan.carr031@gmail.com>
2026-05-29 13:57:49 -0400

    Removing Assembly Version to fix auto-updater not working

--- build-pipeline.bat
+++ build-pipeline.bat
@@ -1,4 +1,5 @@
 cls
+dotnet clean BogChatDesktopClient.sln
 dotnet restore BogChatDesktopClient.sln
 dotnet build --no-restore BogChatDesktopClient.sln -p:Version=1.0.3
 dotnet publish -r win-x64 -f net9.0-windows10.0.22621 --self-contained true -c Release -p:Version=1.0.3 -p:PublishSingleFile=true -p:PublishReadyToRun=true -p:IncludeNativeLibrariesForSelfExtract=true -p:IncludeAllContentForSelfExtract=true -o publish/win-x64 "BogChatDesktopClient/BogChatDesktopClient.csproj"
